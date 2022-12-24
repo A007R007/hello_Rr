1bcaa60a33ce73ca7f15ab4964ccc0e309427f21
ali abbas rajput <aliabbasrajput@yahoo.com>
2022-12-24 11:50:51 -0800

    Few lies added

--- testing123.r
+++ testing123.r
@@ -1,3 +1,9 @@
 ### This is from Github.
 print ("Hello form Git")
 
+name = "Ali"
+Version = "Rstudio - In writing"
+
+print(name)
+print("These lines has been written from Rstudio")
+print(Version)
